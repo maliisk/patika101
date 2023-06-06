a6ab7a51371474032738a5d2808ddf9e91703cd5
Muhammed Ali Işık <isik.mali@outlook.com>
2023-06-06 20:34:49 +0300

    taksimetre

---
+++ src/java101/taksimetre.java
@@ -0,0 +1,22 @@
+package java101;
+
+import java.util.Scanner;
+
+public class taksimetre {
+    public static void main(String[] args) {
+        double mesafe, top=10;
+
+
+        Scanner k = new Scanner(System.in);
+        System.out.println("Km değeri giriniz: ");
+        mesafe=k.nextInt();
+
+        mesafe=mesafe*(2.20);
+        top=top+mesafe;
+
+        top=(top<20) ? 20:top;
+
+        System.out.println("Toplam Ödenecek Tutar : "+ top);
+
+    }
+}
