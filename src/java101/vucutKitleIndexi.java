285e9eddfaefded8202c87ce3ddad15cbec5e593
Muhammed Ali Işık <isik.mali@outlook.com>
2023-06-06 20:51:53 +0300

    6.06.23

--- src/java101/vucutKitleIndexi.java
+++ src/java101/vucutKitleIndexi.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 
 public class vucutKitleIndexi {
     public static void main(String[] args) {
-        double boy,kilo;
+        double boy,kilo,index=0;
         Scanner inp = new Scanner(System.in);
         System.out.println("Lütfen boyunuzu metre cinsinde giriniz : ");
         boy=inp.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz : ");
+        kilo=inp.nextDouble();
+
+        index+=kilo/(boy*boy);
+
+        System.out.println("Vücut Kitle İndeksiniz : "+ index +"'dir");
     }
 }
