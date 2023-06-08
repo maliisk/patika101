1e9cdffd7b13e196017ec9f44e81e6b52421a2ca
Muhammed Ali Işık <isik.mali@outlook.com>
2023-06-08 21:26:14 +0300

    8.06.23

--- src/java101/kullanıcıGirisi.java
+++ src/java101/kullanıcıGirisi.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 public class kullanıcıGirisi {
     public static void main(String[] args) {
         String userName, password,newPassword,cevap;
-        int deger;
 
         Scanner k= new Scanner(System.in);
 
         System.out.println("Lütfen Kullanıcı Adınızı Giriniz : ");
         userName=k.nextLine();
+
         System.out.println("Lütfen Şifrenizi Giriniz : ");
         password=k.nextLine();
 
         if (userName.equals("patika") && password.equals("Patika2023"))
             System.out.println("Tebrikler Giriş Yaptınız !");
+
         else {
             System.out.println("Hatalı Giriş Yaptınız ! \n Şifrenizi sıfırlamak istiyor musunuz) \n (Evet/Hayır)");
             cevap=k.nextLine();
+
             switch (cevap){
                 case "Evet" :
                     System.out.println("Yeni Şifrenizi Giriniz : ");
@@ -27,6 +29,7 @@ public class kullanıcıGirisi {
                         System.out.println("Şifreniz Eski Şifrenizle Aynı Olamaz !");
                     else System.out.println("Yeni Şifreniz Oluşturuldu Tekrardan Giriş Yapınız !");
                     break;
+
                 case "Hayır" :
                     System.out.println("Tekrar Deneyiniz !");
                     break;
