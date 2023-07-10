b47dd97a67a96ed9593b1599a2776afef67c083f
Muhammed Ali Işık <isik.mali@outlook.com>
2023-07-10 18:53:00 +0300

    10.07.23

---
+++ src/employee/Employee.java
@@ -0,0 +1,66 @@
+package employee;
+
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+
+    Employee(String name, double salary, int workHours, int hireYear){
+    this.name=name;
+    this.salary=salary;
+    this.workHours=workHours;
+    this.hireYear=hireYear;
+
+    }
+    public double tax () {
+        double tax = 0;
+        if (salary > 1000) {
+            tax += (salary * 3) / 100;
+        }
+        else {
+            tax = 0;
+        }
+        return tax;
+    }
+    public double bonus () {
+        double bonuss ;
+        if (workHours > 40) {
+            bonuss = (workHours - 40) * 30;
+        }
+        else {
+            bonuss = 0;
+        }
+        return bonuss;
+    }
+    public double raiseSalary () {
+        double increasedSalary = 0;
+        if ((2021 - hireYear) < 10 ) {
+            increasedSalary += (salary * 5) / 100;
+        }
+        if (((2021 - hireYear) > 9) && ((2021 - hireYear) < 20)) {
+            increasedSalary += (salary * 10) / 100;
+        }
+        if ((2021 - hireYear) > 19) {
+            increasedSalary += (salary * 15) / 100;
+        }
+        return increasedSalary;
+    }
+    public void print() {
+        System.out.println("Adı : " + this.name);
+        System.out.println("Salary : " + this.salary);
+        System.out.println("Work hours: " + this.workHours);
+        System.out.println("The year of starting work : " + this.hireYear);
+        System.out.println("Tax : " + tax());
+        System.out.println("Bonus : " + bonus());
+        System.out.println("Salary increase : " + raiseSalary());
+        System.out.println("Salary with taxes and bonus : " + (salary + bonus() - tax()));
+        System.out.println("Total Salary : " + (salary +  raiseSalary()));
+    }
+
+}
+
+
+
+
+
---
+++ src/employee/main.java
@@ -0,0 +1,13 @@
+package employee;
+
+public class main {
+
+    public static void main(String[] args) {
+        Employee Person1 = new Employee("Muhammed",2000,45,2015);
+        Employee Person2 = new Employee("Ali",2000,65,2000);
+        Person1.print();
+        System.out.println();
+        Person2.print();
+    }
+
+}
