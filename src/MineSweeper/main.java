3a9b7620cfa85e26aef901c4a176fddc9cd49a35
Muhammed Ali Işık <isik.mali@outlook.com>
2023-07-25 15:43:08 +0300

    25.07.23

---
+++ src/MineSweeper/main.java
@@ -0,0 +1,10 @@
+package MineSweeper;
+
+public class main {
+
+        public static void main(String[] args) {
+            MineSweeper game = new MineSweeper(10,10);
+            game.run();
+        }
+    }
+
