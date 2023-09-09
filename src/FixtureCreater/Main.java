6bbbd27bcfb7edf40372cf9b5b35786e0059c9c3
Muhammed Ali Işık <isik.mali@outlook.com>
2023-09-09 19:13:08 +0300

    09.09.23

---
+++ src/FixtureCreater/Main.java
@@ -0,0 +1,9 @@
+package FixtureCreater;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Fixture fixtureCreater=new Fixture();
+        fixtureCreater.run();
+    }
+}
